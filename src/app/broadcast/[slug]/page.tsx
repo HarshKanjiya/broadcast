8614c32250d62074fb236b50ee3150d6bc8d2b62
Harsh Kanjiya <harshkanjiya001@gmail.com>
2023-06-18 01:46:06 +0530

    broadcast page initiated

--- src/app/broadcast/[slug]/page.tsx
+++ src/app/broadcast/[slug]/page.tsx
@@ -1,8 +1,30 @@
+import { getAuthSession } from "@/lib/AuthOptions";
+import { db } from "@/lib/db";
 import { FC } from "react";
 
-interface pageProps { }
+interface pageProps {
+  params: {
+    slug: string;
+  };
+}
 
-const page: FC<pageProps> = ({}) => {
+const page = async function ({ params: { slug } }: pageProps) {
+  const session = await getAuthSession();
+
+  const broadcast = await db.broadcast.findFirst({
+    where: { name: slug },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          votes: true,
+          comments: true,
+          Broadcast: true,
+        },
+      },
+    },
+  });
+
   return <div>page</div>;
 };
 
